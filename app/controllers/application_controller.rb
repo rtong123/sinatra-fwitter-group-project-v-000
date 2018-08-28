48ad83b3bf72b79936ee7011b118718509bd1d43
Rebecca Tong <rtong@buffalo.edu>
2018-08-28 01:07:29 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
   end
 
   get '/' do
@@ -13,10 +14,10 @@ class ApplicationController < Sinatra::Base
 
   get '/signup' do
     if !logged_in?
-    erb :'/users/create_user'
-  else
-    redirect '/tweets'
-  end
+      erb :'/users/create_user'
+    else
+      redirect '/tweets'
+    end
   end
 
   post '/signup' do
@@ -24,18 +25,36 @@ class ApplicationController < Sinatra::Base
     if params[:username] != "" && params[:email] != ""  && params[:password] != ""
       @user = User.create(username: params[:username], email: params[:email], password: params[:password])
       session[:user_id] = @user.id
-
       redirect '/tweets'
     else
       redirect '/signup'
+    end
+  end
+
+
+  get '/login' do
+    if logged_in?
+      redirect '/tweets'
+  else
+    erb :'users/login'
   end
 end
 
+  post '/login' do
+    redirect '/tweets'
+  end
 
-get '/login' do
-  erb :'/users/login'
+  get '/logout' do
+    if !logged_in
+    erb :'/login'
+  else
+    erb :'/tweets'
+  end
 end
 
+  post '/logout' do
+
+  end
 
   helpers do
     def logged_in?
@@ -46,6 +65,6 @@ end
       User.find(session[:user_id])
     end
 
-end
+  end
 
 end
--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,6 +1,7 @@
 class TweetsController < ApplicationController
 
   get '/tweets' do
+
     erb :'tweets/tweets'
   end
 
